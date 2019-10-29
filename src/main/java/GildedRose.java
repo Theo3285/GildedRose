a5bb90f0da8aea660bd62942135320cbd1be72ea
Christophe THEPAUT <christophe_thepaut@hotmail.fr>
2019-10-29 23:11:54 +0100

    WIP

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -31,19 +31,19 @@ public class GildedRose {
                 if (item.getQuality() < 50) {
                     item.setQuality(item.getQuality() + 1);
 
-                    if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
-                        if (item.getSellIn() < 11) {
-                            if (item.getQuality() < 50) {
-                                item.setQuality(item.getQuality() + 1);
-                            }
-                        }
-
-                        if (item.getSellIn() < 6) {
-                            if (item.getQuality() < 50) {
-                                item.setQuality(item.getQuality() + 1);
-                            }
-                        }
-                    }
+//                    if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
+//                        if (item.getSellIn() < 11) {
+//                            if (item.getQuality() < 50) {
+//                                item.setQuality(item.getQuality() + 1);
+//                            }
+//                        }
+//
+//                        if (item.getSellIn() < 6) {
+//                            if (item.getQuality() < 50) {
+//                                item.setQuality(item.getQuality() + 1);
+//                            }
+//                        }
+//                    }
                 }
             } else if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
                 if (item.getQuality() < 50) {
