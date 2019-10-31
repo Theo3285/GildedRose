c8aafdc2dc5155ed1c8722813128af36a4ddbe9d
Christophe THEPAUT <christophe_thepaut@hotmail.fr>
2019-10-31 19:14:43 +0100

    WIP

--- src/test/java/GildedRoseShould.java
+++ src/test/java/GildedRoseShould.java
@@ -17,7 +17,7 @@ public class GildedRoseShould {
         items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
         items.add(new Item("Conjured Mana Cake", 3, 6));
 
-
+        GildedRose gildedRose = new GildedRose();
     }
 
 }
