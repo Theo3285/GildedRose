d4e426e3023eafd81050368ead1a8cf9f5dbdfa3
Christophe THEPAUT <christophe_thepaut@hotmail.fr>
2019-10-31 23:05:22 +0100

    WIP

--- src/main/java/ItemUpdater.java
+++ src/main/java/ItemUpdater.java
@@ -7,4 +7,14 @@ public abstract class ItemUpdater {
     }
 
     abstract void updateQuality();
+
+    private void decreaseSellIn(Item item) {
+        item.setSellIn(item.getSellIn() - 1);
+    }
+
+    private void increaseQuality(Item item) {
+        if (item.getQuality() < 50) {
+            item.setQuality(item.getQuality() + 1);
+        }
+    }
 }
