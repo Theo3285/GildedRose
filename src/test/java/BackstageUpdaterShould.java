f6610f0720eebcd5021b7f9b57ea5355821fe1f1
Christophe THEPAUT <christophe_thepaut@hotmail.fr>
2019-10-31 23:02:53 +0100

    WIP

--- src/test/java/BackstageUpdaterShould.java
+++ src/test/java/BackstageUpdaterShould.java
@@ -6,11 +6,11 @@ import static org.junit.Assert.assertThat;
 public class BackstageUpdaterShould {
     @Test
     public void update_quality() {
-        Item item = new Item("",0,0);
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert",0,0);
         BackstageUpdater updater = new BackstageUpdater(item);
 
         updater.updateQuality();
 
-        assertThat(item.toString(), is("Item{name='', sellIn=-1, quality=0}"));
+        assertThat(item.toString(), is("Item{name='Backstage passes to a TAFKAL80ETC concert', sellIn=-1, quality=0}"));
     }
 }
