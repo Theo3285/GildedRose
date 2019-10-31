346ee53aca6f146ef410e81ea8e478cd6bd586fa
Christophe THEPAUT <christophe_thepaut@hotmail.fr>
2019-10-31 22:55:36 +0100

    WIP

--- src/main/java/BackstageUpdater.java
+++ src/main/java/BackstageUpdater.java
@@ -1,7 +1,11 @@
 public class BackstageUpdater {
     private Item item;
 
-    public BackstageUpdater(Item item) {
+    BackstageUpdater(Item item) {
         this.item = item;
+    }
+
+    public void updateQuality() {
+        
     }
 }
