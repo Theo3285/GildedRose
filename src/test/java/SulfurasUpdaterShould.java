c5d1193079bfedd722a851f78c8a535e89c7b82d
Christophe THEPAUT <christophe_thepaut@hotmail.fr>
2019-10-31 23:43:34 +0100

    WIP

--- src/test/java/SulfurasUpdaterShould.java
+++ src/test/java/SulfurasUpdaterShould.java
@@ -1,10 +1,15 @@
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 public class SulfurasUpdaterShould {
     @Test
     public void update_quality() {
         Item item = new Item("Sulfuras, Hand of Ragnaros",0,0);
         ItemUpdater updater = new SulfurasUpdater(item);
         updater.updateQuality();
+
+        assertThat("",is(""));
     }
 }
