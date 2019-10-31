b0954f7030649f01adaf6ec27712ab0886ca4277
Christophe THEPAUT <christophe_thepaut@hotmail.fr>
2019-10-31 21:06:25 +0100

    WIP

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -30,22 +30,20 @@ public class GildedRose {
                     item.setQuality(item.getQuality() + 1);
                 }
             } else if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
+                if (item.getSellIn() < 11) {
+                    if (item.getQuality() < 50) {
+                        item.setQuality(item.getQuality() + 1);
+                    }
+                }
+
+                if (item.getSellIn() < 6) {
+                    if (item.getQuality() < 50) {
+                        item.setQuality(item.getQuality() + 1);
+                    }
+                }
                 if (item.getQuality() < 50) {
                     item.setQuality(item.getQuality() + 1);
 
-//                    if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
-                        if (item.getSellIn() < 11) {
-                            if (item.getQuality() < 50) {
-                                item.setQuality(item.getQuality() + 1);
-                            }
-                        }
-
-                        if (item.getSellIn() < 6) {
-                            if (item.getQuality() < 50) {
-                                item.setQuality(item.getQuality() + 1);
-                            }
-                        }
-  //                  }
                 }
             } else {
                 if ("Sulfuras, Hand of Ragnaros".equals(item.getName())) {
