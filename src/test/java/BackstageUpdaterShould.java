cfad08eda4a4b6b59b4607681cdb137f046c8e46
Christophe THEPAUT <christophe_thepaut@hotmail.fr>
2019-10-31 23:09:07 +0100

    WIP

--- src/test/java/BackstageUpdaterShould.java
+++ src/test/java/BackstageUpdaterShould.java
@@ -7,7 +7,7 @@ public class BackstageUpdaterShould {
     @Test
     public void update_quality() {
         Item item = new Item("Backstage passes to a TAFKAL80ETC concert",0,0);
-        BackstageUpdater updater = new BackstageUpdater(item);
+        ItemUpdater updater = new BackstageUpdater(item);
 
         updater.updateQuality();
 
