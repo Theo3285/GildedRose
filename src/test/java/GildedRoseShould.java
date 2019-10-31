4a67cba79a3de8b4dcaa5d30a841467aa99a5c5f
Christophe THEPAUT <christophe_thepaut@hotmail.fr>
2019-10-31 19:08:13 +0100

    WIP

--- src/test/java/GildedRoseShould.java
+++ src/test/java/GildedRoseShould.java
@@ -1,11 +1,15 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class GildedRoseShould {
 
     @Test
     public void do_something() {
+        List<Item> items = new ArrayList<Item>();
     }
 
 }
