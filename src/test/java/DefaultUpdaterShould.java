5e368ad6926030fae55e5c22c83be50a38c42ac7
Christophe THEPAUT <christophe_thepaut@hotmail.fr>
2019-10-31 23:19:29 +0100

    WIP

--- src/test/java/DefaultUpdaterShould.java
+++ src/test/java/DefaultUpdaterShould.java
@@ -4,5 +4,8 @@ public class DefaultUpdaterShould {
     @Test
     public void update_quality() {
         Item item = new Item("",0,0);
+        ItemUpdater updater = new DefaultUpdater(item);
+
+        updater.updateQuality();
     }
 }
