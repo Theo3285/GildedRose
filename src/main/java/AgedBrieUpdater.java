3ab9e1e5dbccaf30920168f195f37f400d36a6d4
Christophe THEPAUT <christophe_thepaut@hotmail.fr>
2019-10-31 22:29:06 +0100

    WIP

--- src/main/java/AgedBrieUpdater.java
+++ src/main/java/AgedBrieUpdater.java
@@ -8,4 +8,21 @@ public class AgedBrieUpdater {
     public void updateQuality() {
 
     }
+
+    private static void decreaseSellIn(Item item) {
+        item.setSellIn(item.getSellIn() - 1);
+    }
+
+
+    private static void decreaseQuality(Item item) {
+        if (item.getQuality() > 0) {
+            item.setQuality(item.getQuality() - 1);
+        }
+    }
+
+    private static void increaseQuality(Item item) {
+        if (item.getQuality() < 50) {
+            item.setQuality(item.getQuality() + 1);
+        }
+    }
 }
