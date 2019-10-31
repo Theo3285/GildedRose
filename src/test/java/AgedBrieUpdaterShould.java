758374b0e3bbbe268ecec72cf8ea5c8183d3aa25
Christophe THEPAUT <christophe_thepaut@hotmail.fr>
2019-10-31 22:27:48 +0100

    WIP

--- src/test/java/AgedBrieUpdaterShould.java
+++ src/test/java/AgedBrieUpdaterShould.java
@@ -1,5 +1,8 @@
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 public class AgedBrieUpdaterShould {
     @Test
     public void update_quality() {
@@ -8,5 +11,7 @@ public class AgedBrieUpdaterShould {
         AgedBrieUpdater updater = new AgedBrieUpdater(item);
 
         updater.updateQuality();
+
+        assertThat(item.toString(), is("Item{name='', sellIn=0, quality=0}"));
     }
 }
