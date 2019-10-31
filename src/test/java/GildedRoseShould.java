df7826d3d16c548cbb9d5041344f7dcae929d109
Christophe THEPAUT <christophe_thepaut@hotmail.fr>
2019-10-31 19:46:15 +0100

    WIP

--- src/test/java/GildedRoseShould.java
+++ src/test/java/GildedRoseShould.java
@@ -24,16 +24,8 @@ public class GildedRoseShould {
     @Test
     public void update_quality() {
         List<Item> items = new ArrayList<Item>();
-        items.add(new Item("+5 Dexterity Vest", 10, 20));
-        items.add(new Item("Aged Brie", 2, 0));
-        items.add(new Item("Elixir of the Mongoose", 5, 7));
-        items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
-        items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
-        items.add(new Item("Conjured Mana Cake", 3, 6));
-
-        List<Item> tempItems = new ArrayList<Item>();
         for (int i = 0; i < 2000; i++) {
-            tempItems.add(new Item(itemName(), sellIn(), quality()));
+            items.add(new Item(itemName(), sellIn(), quality()));
         }
 
         GildedRose gildedRose = new GildedRose();
