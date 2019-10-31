88a4e5767bcd799fc8cac82fab2a163ac328f0c3
Christophe THEPAUT <christophe_thepaut@hotmail.fr>
2019-10-31 22:55:01 +0100

    WIP

--- src/test/java/BackstageUpdaterShould.java
+++ src/test/java/BackstageUpdaterShould.java
@@ -4,5 +4,6 @@ public class BackstageUpdaterShould {
     @Test
     public void update_quality() {
         Item item = new Item("",0,0);
+        BackstageUpdater updater = new BackstageUpdater(item);
     }
 }
