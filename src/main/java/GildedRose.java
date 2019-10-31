9238aaf8ff969d43cd567744f50de02007c0d72d
Christophe THEPAUT <christophe_thepaut@hotmail.fr>
2019-10-31 21:02:12 +0100

    WIP

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -51,30 +51,33 @@ public class GildedRose {
                 }
             } else {
                 if (item.getQuality() > 0) {
-                    if (!"Sulfuras, Hand of Ragnaros".equals(item.getName())) {
+                    if ("Sulfuras, Hand of Ragnaros".equals(item.getName())) {
+                    } else {
                         item.setQuality(item.getQuality() - 1);
                     }
                 }
             }
 
-            if (!"Sulfuras, Hand of Ragnaros".equals(item.getName())) {
+            if ("Sulfuras, Hand of Ragnaros".equals(item.getName())) {
+            } else {
                 item.setSellIn(item.getSellIn() - 1);
             }
 
             if (item.getSellIn() < 0) {
-                if (!"Aged Brie".equals(item.getName())) {
-                    if (!"Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
-                        if (item.getQuality() > 0) {
-                            if (!"Sulfuras, Hand of Ragnaros".equals(item.getName())) {
-                                item.setQuality(item.getQuality() - 1);
-                            }
-                        }
-                    } else {
-                        item.setQuality(item.getQuality() - item.getQuality());
-                    }
-                } else {
+                if ("Aged Brie".equals(item.getName())) {
                     if (item.getQuality() < 50) {
                         item.setQuality(item.getQuality() + 1);
+                    }
+                } else {
+                    if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
+                        item.setQuality(item.getQuality() - item.getQuality());
+                    } else {
+                        if (item.getQuality() > 0) {
+                            if ("Sulfuras, Hand of Ragnaros".equals(item.getName())) {
+                                continue;
+                            }
+                            item.setQuality(item.getQuality() - 1);
+                        }
                     }
                 }
             }
