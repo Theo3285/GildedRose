80edf23bdcce35a48ecf31d4c0c877c114f26470
Christophe THEPAUT <christophe_thepaut@hotmail.fr>
2019-10-31 23:01:51 +0100

    WIP

--- src/test/java/BackstageUpdaterShould.java
+++ src/test/java/BackstageUpdaterShould.java
@@ -11,6 +11,6 @@ public class BackstageUpdaterShould {
 
         updater.updateQuality();
 
-        assertThat("Item{name='', sellIn=-1, quality=2}", is("Item{name='', sellIn=-1, quality=2}"));
+        assertThat(item.toString(), is("Item{name='', sellIn=-1, quality=0}"));
     }
 }
