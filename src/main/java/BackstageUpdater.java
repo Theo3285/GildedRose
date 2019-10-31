77ec2275617036c8a73b2ee3e156909f3dd0b80c
Christophe THEPAUT <christophe_thepaut@hotmail.fr>
2019-10-31 22:57:25 +0100

    WIP

--- src/main/java/BackstageUpdater.java
+++ src/main/java/BackstageUpdater.java
@@ -8,4 +8,14 @@ public class BackstageUpdater {
     public void updateQuality() {
 
     }
+
+    private static void decreaseSellIn(Item item) {
+        item.setSellIn(item.getSellIn() - 1);
+    }
+
+    private static void increaseQuality(Item item) {
+        if (item.getQuality() < 50) {
+            item.setQuality(item.getQuality() + 1);
+        }
+    }
 }
