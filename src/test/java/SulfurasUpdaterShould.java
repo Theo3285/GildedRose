711c2c82247b13834be36deb3fd022bb05dc81a4
Christophe THEPAUT <christophe_thepaut@hotmail.fr>
2019-10-31 23:30:21 +0100

    WIP

--- src/test/java/SulfurasUpdaterShould.java
+++ src/test/java/SulfurasUpdaterShould.java
@@ -3,6 +3,6 @@ import org.junit.Test;
 public class SulfurasUpdaterShould {
     @Test
     public void update_quality() {
-        Item item = new Item("",0,0);
+        Item item = new Item("Sulfuras, Hand of Ragnaros",0,0);
     }
 }
