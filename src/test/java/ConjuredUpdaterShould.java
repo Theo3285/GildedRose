740f7e28023b6a31b0224426affcd236b61612bf
Christophe THEPAUT <christophe_thepaut@hotmail.fr>
2019-11-01 09:31:07 +0100

    WIP

--- src/test/java/ConjuredUpdaterShould.java
+++ src/test/java/ConjuredUpdaterShould.java
@@ -6,12 +6,12 @@ import static org.junit.Assert.assertThat;
 public class ConjuredUpdaterShould {
     @Test
     public void decrease_quality_twice_as_fast_as_normal_items() {
-        Item item = new Item("Elixir of the Mongoose",0,4);
+        Item item = new Item("Elixir of the Mongoose",2,4);
         ItemUpdater updater = new NormalUpdater(item);
 
         updater.updateQuality();
 
-        assertThat(item.toString(), is("Item{name='Elixir of the Mongoose', sellIn=-1, quality=2}"));
+        assertThat(item.toString(), is("Item{name='Elixir of the Mongoose', sellIn=1, quality=3}"));
 
     }
 }
