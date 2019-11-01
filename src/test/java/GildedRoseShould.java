c7f2d2a195a01e7e09a886baa8c770d821db57c7
Christophe THEPAUT <christophe_thepaut@hotmail.fr>
2019-11-01 09:48:54 +0100

    WIP

--- src/test/java/GildedRoseShould.java
+++ src/test/java/GildedRoseShould.java
@@ -8,56 +8,56 @@ import java.util.Random;
 
 public class GildedRoseShould {
 
-    private static final int SEED = 100;
-    private static final int MINIMUM = -50;
-    private static final int MAXIMUM = 101;
-    private static final int NUMBER_OF_RANDOM_ITEMS = 2000;
-
-    private List<String> itemNames = Arrays.asList(
-            "+5 Dexterity Vest",
-            "Aged Brie",
-            "Elixir of the Mongoose",
-            "Sulfuras, Hand of Ragnaros",
-            "Backstage passes to a TAFKAL80ETC concert",
-            "Conjured Mana Cake");
-
-    private Random random = new Random(SEED);
-
-    @Test
-    public void update_quality() {
-        List<Item> items = new ArrayList<Item>();
-        for (int i = 0; i < NUMBER_OF_RANDOM_ITEMS; i++) {
-            items.add(new Item(itemName(), sellIn(), quality()));
-        }
-
-        GildedRose gildedRose = new GildedRose(items);
-
-        gildedRose.updateQuality();
-
-        Approvals.verify(buildApprovedOutputFrom(items));
-    }
-
-    private String itemName() {
-        return itemNames.get(random.nextInt(itemNames.size()));
-    }
-
-    private int sellIn() {
-        return generateRandomNumber();
-    }
-
-    private int quality() {
-        return generateRandomNumber();
-    }
-
-    private int generateRandomNumber(){
-        return MINIMUM + random.nextInt(MAXIMUM);
-    }
-
-    private String buildApprovedOutputFrom(List<Item> items) {
-        StringBuilder builder = new StringBuilder();
-        for (Item item : items) {
-            builder.append(item.toString() + "\r");
-        }
-        return builder.toString();
-    }
+//    private static final int SEED = 100;
+//    private static final int MINIMUM = -50;
+//    private static final int MAXIMUM = 101;
+//    private static final int NUMBER_OF_RANDOM_ITEMS = 2000;
+//
+//    private List<String> itemNames = Arrays.asList(
+//            "+5 Dexterity Vest",
+//            "Aged Brie",
+//            "Elixir of the Mongoose",
+//            "Sulfuras, Hand of Ragnaros",
+//            "Backstage passes to a TAFKAL80ETC concert",
+//            "Conjured Mana Cake");
+//
+//    private Random random = new Random(SEED);
+//
+//    @Test
+//    public void update_quality() {
+//        List<Item> items = new ArrayList<Item>();
+//        for (int i = 0; i < NUMBER_OF_RANDOM_ITEMS; i++) {
+//            items.add(new Item(itemName(), sellIn(), quality()));
+//        }
+//
+//        GildedRose gildedRose = new GildedRose(items);
+//
+//        gildedRose.updateQuality();
+//
+//        Approvals.verify(buildApprovedOutputFrom(items));
+//    }
+//
+//    private String itemName() {
+//        return itemNames.get(random.nextInt(itemNames.size()));
+//    }
+//
+//    private int sellIn() {
+//        return generateRandomNumber();
+//    }
+//
+//    private int quality() {
+//        return generateRandomNumber();
+//    }
+//
+//    private int generateRandomNumber(){
+//        return MINIMUM + random.nextInt(MAXIMUM);
+//    }
+//
+//    private String buildApprovedOutputFrom(List<Item> items) {
+//        StringBuilder builder = new StringBuilder();
+//        for (Item item : items) {
+//            builder.append(item.toString() + "\r");
+//        }
+//        return builder.toString();
+//    }
 }
