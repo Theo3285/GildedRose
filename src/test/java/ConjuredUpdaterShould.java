34b8f9b75b513d0d3141f43650b9adc1c1201c9a
Christophe THEPAUT <christophe_thepaut@hotmail.fr>
2019-11-01 09:27:03 +0100

    WIP

--- src/test/java/ConjuredUpdaterShould.java
+++ src/test/java/ConjuredUpdaterShould.java
@@ -1,8 +1,17 @@
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 public class ConjuredUpdaterShould {
     @Test
     public void decrease_quality_twice_as_fast_as_normal_items() {
-        
+        Item item = new Item("Elixir of the Mongoose",0,1);
+        ItemUpdater updater = new NormalUpdater(item);
+
+        updater.updateQuality();
+
+        assertThat(item.toString(), is("Item{name='Elixir of the Mongoose', sellIn=-1, quality=0}"));
+
     }
 }
