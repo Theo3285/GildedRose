4f3fdcfb8a53c38fd191d94cf32f59319330b1a9
Christophe THEPAUT <christophe_thepaut@hotmail.fr>
2019-11-01 09:21:16 +0100

    WIP

--- src/main/java/ConjuredUpdater.java
+++ src/main/java/ConjuredUpdater.java
@@ -5,6 +5,6 @@ public class ConjuredUpdater extends ItemUpdater{
 
     @Override
     void updateQuality() {
-        
+
     }
 }
--- src/test/java/ConjuredUpdaterShould.java
+++ src/test/java/ConjuredUpdaterShould.java
@@ -1,2 +1,7 @@
+import org.junit.Test;
+
 public class ConjuredUpdaterShould {
+    @Test
+    public void decrease_quality_twice_as_fast() {
+    }
 }
