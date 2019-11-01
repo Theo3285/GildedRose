0c1012be4a1b5d4751ecc0283f5f6c90d715380c
Christophe THEPAUT <christophe_thepaut@hotmail.fr>
2019-11-01 09:21:44 +0100

    WIP

--- src/test/java/ConjuredUpdaterShould.java
+++ src/test/java/ConjuredUpdaterShould.java
@@ -2,6 +2,6 @@ import org.junit.Test;
 
 public class ConjuredUpdaterShould {
     @Test
-    public void decrease_quality_twice_as_fast() {
+    public void decrease_quality_twice_as_fast_as_normal_items() {
     }
 }
